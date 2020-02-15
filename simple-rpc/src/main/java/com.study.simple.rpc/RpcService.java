3d701ffd8d51f61fcd26c8e94e7dddbef3be344e
LostLegend <forsakenzero@163.com>
2020-02-15 15:04:44 +0800

    Commit first java file test.

---
+++ simple-rpc/src/main/java/com.study.simple.rpc/RpcService.java
@@ -0,0 +1,11 @@
+package com.study.simple.rpc;
+
+/**
+ * The simplest rpc service. Only include java serial and socket connection.
+ *
+ * @author : lost legend  2020.02.15  15:00
+ * @since V1.0
+ */
+public interface RpcService {
+
+}
